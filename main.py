685ba5e7fd3c74edb7d8e0ecbdd7cd2dedcab1fb
art7235 <art7235@mail.ru>
2024-11-02 12:19:57 +0300

    исправил ошибку

--- main.py
+++ main.py
@@ -180,13 +180,13 @@ class Mob(pygame.sprite.Sprite):
             self.rect = self.image.get_rect()
             self.rect.center = old_center
 
-    # def update(self):
-    #     self.rotate()
-    #     self.image = pygame.transform.rotate(self.image, self.rot_speed)
-    #     self.rect.x += self.speedx
-    #     self.rect.y += self.speedy
-    #     if self.rect.top > HEIGHT + 10 or self.rect.left < -25 or self.rect.right > WIDTH + 20:
-    #         self.rect.x = random.randrange(WIDTH - self.rect.width)
+    def update(self):
+        self.rotate()
+        self.image = pygame.transform.rotate(self.image, self.rot_speed)
+        self.rect.x += self.speedx
+        self.rect.y += self.speedy
+        if self.rect.top > HEIGHT + 10 or self.rect.left < -25 or self.rect.right > WIDTH + 20:
+            self.rect.x = random.randrange(WIDTH - self.rect.width)
             self.rect.y = random.randrange(-100, -40)
             self.speedy = random.randrange(1, 8)
 
